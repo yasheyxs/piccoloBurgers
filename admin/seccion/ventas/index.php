36122bce9942f114a86ccaf0bacd1dbb27792372
yasheyxs <jazmingaidoyxs@gmail.com>
2025-09-02 04:28:36 -0300

    commit32: Añade nombres de meses para la visualización en Chart.js en la sección de ventas.

--- admin/seccion/ventas/index.php
+++ admin/seccion/ventas/index.php
@@ -65,7 +65,16 @@ $ventas_mensuales = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // Datos para Chart.js
 $productos_labels = json_encode(array_column($productos_grafico, 'nombre'));
 $productos_data = json_encode(array_column($productos_grafico, 'total_vendido'));
-$meses = json_encode(array_map(function($v){ return 'Mes '.$v['mes']; }, $ventas_mensuales));
+$mes_nombres = [
+    1 => 'Enero', 2 => 'Febrero', 3 => 'Marzo', 4 => 'Abril',
+    5 => 'Mayo', 6 => 'Junio', 7 => 'Julio', 8 => 'Agosto',
+    9 => 'Septiembre', 10 => 'Octubre', 11 => 'Noviembre', 12 => 'Diciembre'
+];
+
+$meses = json_encode(array_map(function($v) use ($mes_nombres) {
+    return $mes_nombres[(int)$v['mes']] ?? 'Mes ' . $v['mes'];
+}, $ventas_mensuales));
+
 $ventas_data = json_encode(array_column($ventas_mensuales, 'total_ventas'));
 
 include("../../templates/header.php");
